a1a67d628807a8e92e835e22b1e1de68b7a359d6
Taimai14 <maiduytai2002@gmail.com>
2024-02-22 14:23:38 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const cors = require("cors");
+const ApiError = require("./app/api.-error");
+const contactsRouter = require("./app/routes/contact.route");
 
 const app = express();
-const contactsRouter = require("./app/routes/contact.route");
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
+
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+  return res.status(errot.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+  });
+});
 
 app.get("/", (req, res) => [
   res.send({ message: "Welcome to contacct book application." }),
