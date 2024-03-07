c91eac57f017086d58bc16e282f3229d2b097372
Taimai14 <maiduytai2002@gmail.com>
2024-03-07 15:48:30 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -15,15 +15,13 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  return res.status(errot.statusCode || 500).json({
-    message: error.message || "Internal Server Error",
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
   });
 });
 
 app.get("/", (req, res) => [
   res.send({ message: "Welcome to contacct book application." }),
 ]);
-
-app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
